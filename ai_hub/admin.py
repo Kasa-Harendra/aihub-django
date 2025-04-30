cca767d30d7cf955174986e5363eb0fde649fe4d
Harendra Kasa <harendrakasa@gmail.com>
2025-04-30 14:21:45 +0530

    First commit

---
+++ ai_hub/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from . import models
+
+# Register your models here.
+admin.site.register(models.Team)
+admin.site.register(models.Apps)
+admin.site.register(models.Game)
+admin.site.register(models.Event)
+admin.site.register(models.Blog)
---
+++ ai_hub/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.utils import timezone
+import os
+
+# Create your models here.
+class Team(models.Model):
+    s_id = models.CharField(max_length=10, blank=True, null=True)
+    name = models.CharField(max_length=30)
+    position = models.CharField(max_length=30)
+    img = models.ImageField(upload_to='team_images')
+    git = models.URLField(max_length=255, blank=True, null=True)
+    lkdn = models.URLField(max_length=255, blank=True, null=True)
+    mail = models.EmailField(max_length=255, blank=True, null=True)
+    
+class Apps(models.Model):
+    title = models.CharField(max_length=30, blank=True, null=True)
+    desc = models.TextField(max_length=500, blank=True, null=True)
+    img = models.ImageField(upload_to='apps_images', blank=True, null=True)
+    name = models.CharField(max_length=30, blank=True, null=True)
+    s_id = models.CharField(max_length=15, blank=True, null=True)
+    link = models.URLField(max_length=255, blank=True, null=True)
+        
+class Game(models.Model):
+    title = models.CharField(max_length=30, blank=True, null=True)
+    name = models.CharField(max_length=30, blank=True, null=True)
+    s_id = models.CharField(max_length=15, blank=True, null=True)
+    desc = models.TextField(max_length=500, blank=True, null=True)
+    img = models.ImageField(upload_to='game_images', blank=True, null=True)
+    git_link = models.URLField(max_length=300, blank=True, null=True)
+    play_url = models.URLField(max_length=300, blank=True, null=True)
+    
+class Event(models.Model):
+    topic = models.CharField(max_length=100, blank=True, null=True)
+    speaker = models.CharField(max_length=30, blank=True, null=True)
+    speaker_details = models.TextField(max_length=500, blank=True, null=True)
+    desc = models.TextField(max_length=500, blank=True, null=True)
+    schedule = models.DateTimeField(blank=True, null=True)
+    meet_url = models.URLField(max_length=500, blank=True, null=True)
+    vid_url = models.URLField(max_length=500, blank=True, null=True)
+    
+    def is_upcoming(self):
+        if self.schedule:
+            return self.schedule > timezone.now()
+        return False
+    
+class Blog(models.Model):
+    title = models.CharField(max_length=250)
+    blog = models.FileField(upload_to='blogs', blank=True, null=True)
+    def get_file_name(self):
+        if self.blog and self.blog.url:
+            base, ext = os.path.splitext(self.blog.url)
+            return base
+        return self.blog.url
